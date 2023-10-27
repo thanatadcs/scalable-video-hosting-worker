959fa4a2001fdb1e948219cbbbab33fe6e7306e6
thanatadcs <thanatad.cs@gmail.com>
2023-10-28 01:01:20 +0700

    Add sample video conversion using ffmpeg

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,29 @@
 package com.example;
 
-public class Main
-{
-    public static void main( String[] args )
-    {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    static S3Service s3Service = new S3Service();
+
+    public static void main(String[] args) throws IOException {
         while (true) {
             TaskQueueService taskQueueService = new TaskQueueService();
-            System.out.println(taskQueueService.getTask());
+            String fileName = taskQueueService.getTask();
+            System.out.println(fileName);
+            String downloadPath = "original";
+            s3Service.downloadFile("scalable-p2", fileName + "/original", downloadPath);
+            Process p = new ProcessBuilder("ffmpeg", "-i", downloadPath, "convert.mp4").start();
+            InputStream in = p.getErrorStream();
+            InputStreamReader inr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(inr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
         }
     }
 }
