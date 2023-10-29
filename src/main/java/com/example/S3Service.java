7adbe9613a8894b6b55a55db3225263c87f2c310
thanatadcs <thanatad.cs@gmail.com>
2023-10-29 12:04:52 +0700

    Add upload to s3 to S3Service

--- src/main/java/com/example/S3Service.java
+++ src/main/java/com/example/S3Service.java
@@ -1,19 +1,30 @@
 package com.example;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
 import software.amazon.awssdk.transfer.s3.model.CompletedFileDownload;
 import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
 import software.amazon.awssdk.transfer.s3.model.FileDownload;
 import software.amazon.awssdk.transfer.s3.progress.LoggingTransferListener;
 
+import java.io.File;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
 public class S3Service {
 
+    private static S3Client s3Client = S3Client.create();
     private S3TransferManager transferManager = S3TransferManager.create();
 
     /*
-     * source: https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_GetObject_section.html
+     * Source: https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_GetObject_section.html
      */
     public Long downloadFile(String bucketName, String key, String downloadedFileWithPath) {
         DownloadFileRequest downloadFileRequest =
@@ -27,5 +38,25 @@ public class S3Service {
 
         CompletedFileDownload downloadResult = downloadFile.completionFuture().join();
         return downloadResult.response().contentLength();
+    }
+
+    /*
+     * Source: https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_PutObject_section.html
+     */
+    public static void putS3Object(String bucketName, String objectKey, String objectPath) {
+        try {
+            Map<String, String> metadata = new HashMap<>();
+            PutObjectRequest putOb = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectKey)
+                    .metadata(metadata)
+                    .build();
+
+            s3Client.putObject(putOb, RequestBody.fromFile(new File(objectPath)));
+            System.out.println("Successfully placed " + objectKey + " into bucket " + bucketName);
+        } catch (S3Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
