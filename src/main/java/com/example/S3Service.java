628d06842af1eeee05f1f17ae6af5a6403dad787
thanatadcs <thanatad.cs@gmail.com>
2023-12-02 23:27:17 +0700

    Change S3 service to use transferManager for file upload

--- src/main/java/com/example/S3Service.java
+++ src/main/java/com/example/S3Service.java
@@ -1,26 +1,21 @@
 package com.example;
 
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
-import software.amazon.awssdk.transfer.s3.model.CompletedFileDownload;
-import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
-import software.amazon.awssdk.transfer.s3.model.FileDownload;
+import software.amazon.awssdk.transfer.s3.model.*;
 import software.amazon.awssdk.transfer.s3.progress.LoggingTransferListener;
 
-import java.io.File;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 public class S3Service {
 
-    private static S3Client s3Client = S3Client.create();
-    private S3TransferManager transferManager = S3TransferManager.create();
+    private S3TransferManager transferManager;
+
+    S3Service() {
+        transferManager = S3TransferManager.create();
+    }
 
     /*
      * Source: https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_GetObject_section.html
@@ -40,22 +35,17 @@ public class S3Service {
     }
 
     /*
-     * Source: https://docs.aws.amazon.com/AmazonS3/latest/userguide/example_s3_PutObject_section.html
+     * Source: https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/transfer/s3/S3TransferManager.html
      */
-    public static void putS3Object(String bucketName, String objectKey, String objectPath) {
-        try {
-            Map<String, String> metadata = new HashMap<>();
-            PutObjectRequest putOb = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(objectKey)
-                    .metadata(metadata)
-                    .build();
+    public void uploadFile(String bucketName, String objectKey, String objectPath) {
+        UploadFileRequest uploadFileRequest = UploadFileRequest.builder()
+                .putObjectRequest(req -> req.bucket(bucketName).key(objectKey))
+                .addTransferListener(LoggingTransferListener.create())
+                .source(Paths.get(objectPath))
+                .build();
 
-            s3Client.putObject(putOb, RequestBody.fromFile(new File(objectPath)));
-            System.out.println("Successfully placed " + objectKey + " into bucket " + bucketName);
-        } catch (S3Exception e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+        FileUpload upload = transferManager.uploadFile(uploadFileRequest);
+        upload.completionFuture().join();
     }
+
 }
